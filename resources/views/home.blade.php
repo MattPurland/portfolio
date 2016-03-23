e9c633166686cee2e7bd917f8bb79a9161fb3bdf
Matthew Purland <matthew.purland@monogramdigital.com>
2016-03-23 22:01:26 +0000

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,208 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Matthew Purland - Portfolio</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/app.css">
+    </head>
+    <body>
+        <header class="row">
+            <div class="small-12">
+                <h1>Matthew Purland</h1>
+                <h4>Web Development Portfolio</h4>
+            </div>
+        </header>
+
+        <section id="introduction">
+            <div class="row medium-centered">
+                <div class="small-12 medium-10 large-8 medium-push-1 large-push-2">
+                    <p>
+                        I'm an ambitious and determined web developer with a passion for code, modern design and using 
+                        the latest technologies available to build and maintain slick and beautiful websites.
+                    </p>
+                    <p>
+                        Below is a selection of projects I've worked on in my career, with the majority of the work done 
+                        by myself. I've also worked on many other projects doing amendments, contributions and private projects.
+                    </p>
+                        Please get in touch with any questions, 
+                        queries or job opportunities:
+                    </p>
+                    <div class="contact">
+                        <ul>
+                            <li>
+                                <span class="icon">&#xe08e;</span>
+                                <a href="tel:01508813295">01508 813295</a>
+                            </li>
+                            <li>
+                                <span class="icon">&#xe08e;</span>
+                                <a href="tel:07748191310">07748 191 310</a>
+                            </li>
+                            <li>
+                                <span class="icon">&#xe10f;</span>
+                                <a href="mailto:mattpurland@gmail.com">mattpurland@gmail.com</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="portfolio">
+            <div class="row">
+                <!-- Greg Rusedski -->
+                <div class="small-12 columns medium-6 large-4 item">
+                    <div class="container">
+                        <div class="background" style="background-image: url(images/sites/gregrusedski.png)"></div>
+                        <div class="overlay"></div>
+                        <div class="information">
+                            <img src="images/sites/gregrusedski.logo.png" />
+                            <h5>Greg Rusedski</h5>
+                            <a href="http://www.gregrusedski.com" target="_blank">Open site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Magic Steps Nursery -->
+                <div class="small-12 columns medium-6 large-4 item">
+                    <div class="container">
+                        <div class="background" style="background-image: url(images/sites/msn.png)"></div>
+                        <div class="overlay"></div>
+                        <div class="information">
+                            <img src="images/sites/msn.logo.png" />
+                            <h5>Magic Steps Nursery</h5>
+                            <a href="http://www.magicstepsnursery.co.uk" target="_blank">Open site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- 1.4 -->
+                <div class="small-12 columns medium-6 large-4 item">
+                    <div class="container">
+                        <div class="background" style="background-image: url(images/sites/onepointfour.png)"></div>
+                        <div class="overlay"></div>
+                        <div class="information">
+                            <img src="images/sites/onepointfour.logo.png" />
+                            <h5>1.4</h5>
+                            <a href="http://www.onepointfour.co" target="_blank">Open site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Thy Stride -->
+                <div class="small-12 columns medium-6 large-4 item">
+                    <div class="container">
+                        <div class="background" style="background-image: url(images/sites/thystride.png)"></div>
+                        <div class="overlay"></div>
+                        <div class="information">
+                            <img src="images/sites/thystride.logo.png" />
+                            <h5>1.4</h5>
+                            <a href="http://www.thystride.com" target="_blank">Open site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Pat Cash -->
+                <div class="small-12 columns medium-6 large-4 item">
+                    <div class="container">
+                        <div class="background" style="background-image: url(images/sites/patcash.png)"></div>
+                        <div class="overlay"></div>
+                        <div class="information">
+                            <img src="images/sites/patcash.logo.png" />
+                            <h5>Pat Cash</h5>
+                            <a href="http://www.patcash.co.uk" target="_blank">Open site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- AutoTrader Vauxhall NetworkQ -->
+                <div class="small-12 columns medium-6 large-4 item">
+                    <div class="container">
+                        <div class="background" style="background-image: url(images/sites/at-networkq.png)"></div>
+                        <div class="overlay"></div>
+                        <div class="information">
+                            <img src="images/sites/at-networkq.logo.png" />
+                            <h5>AutoTrader / Vauxhall NetworkQ</h5>
+                            <a href="http://promotions.autotrader.co.uk/vauxhall-networkq" target="_blank">Open site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- AutoTrader Infiniti -->
+                <div class="small-12 columns medium-6 large-4 item">
+                    <div class="container">
+                        <div class="background" style="background-image: url(images/sites/at-infiniti.png)"></div>
+                        <div class="overlay"></div>
+                        <div class="information">
+                            <img src="images/sites/at-infiniti.logo.png" />
+                            <h5>AutoTrader / Infiniti</h5>
+                            <a href="http://promotions.autotrader.co.uk/infiniti" target="_blank">Open site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- AutoTrader Nissan Leaf -->
+                <div class="small-12 columns medium-6 large-4 item">
+                    <div class="container">
+                        <div class="background" style="background-image: url(images/sites/at-nissan-leaf.png)"></div>
+                        <div class="overlay"></div>
+                        <div class="information">
+                            <img src="images/sites/at-nissan-leaf.logo.png" />
+                            <h5>AutoTrader / Nissan Leaf</h5>
+                            <a href="http://promotions.autotrader.co.uk/nissan-leaf" target="_blank">Open site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- BandApp-->
+                <div class="small-12 columns medium-6 large-4 item">
+                    <div class="container">
+                        <div class="background" style="background-image: url(images/sites/bandapp.png)"></div>
+                        <div class="overlay"></div>
+                        <div class="information">
+                            <img src="images/sites/bandapp.logo.png" />
+                            <h5>BandApp</h5>
+                            <a href="http://www.bandapp.com" target="_blank">Open site</a>
+                        </div>
+                    </div>
+                </div>
+
+                 <!-- TDTP -->
+                <div class="small-12 columns medium-6 large-4 item">
+                    <div class="container">
+                        <div class="background" style="background-image: url(images/sites/tdtp.png)"></div>
+                        <div class="overlay"></div>
+                        <div class="information">
+                            <img src="images/sites/tdtp.logo.png" />
+                            <h5>The Digital Transformation People</h5>
+                            <a href="http://www.thedigitaltransformationpeople.com" target="_blank">Open site</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- TBDP -->
+                <div class="small-12 columns medium-6 large-4 item">
+                    <div class="container">
+                        <div class="background" style="background-image: url(images/sites/tbdp.png)"></div>
+                        <div class="overlay"></div>
+                        <div class="information">
+                            <img src="images/sites/tbdp.logo.png" />
+                            <h5>The Big Data People</h5>
+                            <a href="http://www.thebigdatapeople.com" target="_blank">Open site</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <script src="bower_components/jquery/dist/jquery.min.js"></script>
+        <script src="bower_components/foundation/js/foundation.min.js"></script>
+        <script>
+            $(document).foundation();
+            $(document).ready(function() {
+                $('body').css('opacity', 1);
+            });
+        </script>
+    </body>
+</html>
